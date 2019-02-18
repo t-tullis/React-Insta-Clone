0d9430facdb0555a3f1c2ea1c5aae65da1382b15
t-tullis <t.tullis34@gmail.com>
2019-02-18 11:16:29 -0800

    initialized branch, removed files, and set up boilerplate

---
+++ instaclone/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Set up CRA Boilerplate!
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
